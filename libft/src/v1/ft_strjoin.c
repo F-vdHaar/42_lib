d28750f66e51f8fde96629c89323e5d392ec693a
fvon-der <fvon-der@student.42heilbronn.de>
2025-04-23 19:15:09 +0200

    DEBUG ft_strjoin
    Memory Management

--- libft/src/v1/ft_strjoin.c
+++ libft/src/v1/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fvon-der <fvon-der@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 20:11:17 by fvon-der          #+#    #+#             */
-/*   Updated: 2024/10/08 20:11:20 by fvon-der         ###   ########.fr       */
+/*   Updated: 2025/04/23 19:14:07 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,26 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*result;
-	int		i;
-	int		j;
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
+	size_t	j;
 
+	len1 = 0;
+	len2 = 0;
+	if (s1)
+		len1 = ft_strlen(s1);
+	if (s2)
+		len2 = ft_strlen(s2);
+	result = (char *)ft_calloc(sizeof(char), len1 + len2 + 1);
+	if (!result)
+		return (NULL);
 	i = 0;
 	j = 0;
-	result = (char *) ft_calloc(sizeof(char), ft_strlen(s1) + ft_strlen(s2)+1);
-	if (!result || (!s1 && !s2))
-		return (NULL);
-	while (s1[i])
+	while (s1 && s1[i])
 		result[j++] = s1[i++];
-	i = 0 ;
-	while (s2[i])
+	i = 0;
+	while (s2 && s2[i])
 		result[j++] = s2[i++];
 	result[j] = '\0';
 	return (result);
